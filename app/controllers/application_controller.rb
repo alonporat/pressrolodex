94c94c964afc92b25fe59b113260d42cdee9fc7f
Alon Porat <porat.alon@gmail.com>
2013-10-28 14:02:54 +0200

    fix sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ before_filter :configure_permitted_parameters, if: :devise_controller?
 protected
 
 	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) << :FirstName
+		devise_parameter_sanitizer.for(:sign_up) << :LastName
+		devise_parameter_sanitizer.for(:sign_up) << :Nickname
 		devise_parameter_sanitizer.for(:account_update) << :FirstName
 		devise_parameter_sanitizer.for(:account_update) << :LastName
 		devise_parameter_sanitizer.for(:account_update) << :Nickname
